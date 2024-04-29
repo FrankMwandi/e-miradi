3a5fcdeb1a47822eeccee969cb3dc663815b97d6
mwandi <frankmwandi93@gmail.com>
2024-04-29 23:44:05 +0300

    adding dashboard view

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import login , authenticate
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
+
 def user_login(request):
-    if request == 'POST':
+    if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -24,3 +26,10 @@ def user_login(request):
     else:
         form = LoginForm()
         return render(request, 'account/login.html', {'form':form})
+
+#Login required 
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section':'dashboard'})
