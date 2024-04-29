72380f3678d480e883329a715610064bb06c4b25
mwandi <frankmwandi93@gmail.com>
2024-04-29 23:42:51 +0300

    using django auth and dashboard views to easy login and logout

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
 from . import views 
 
 urlpatterns = [
-    path('login/', views.user_login, name='login'),
+    # using custom viewpath('login/', views.user_login, name='login'),
+    path('login/', auth_view.LoginView.as_view(), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard' ),
 ]
