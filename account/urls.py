7c265915856c1db4c86dd58a05a8983bd5881da9
mwandi <frankmwandi93@gmail.com>
2024-05-02 21:44:59 +0300

    using custom logout

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
 from . import views 
+from .views import user_logout
 
 urlpatterns = [
     # using custom viewpath('login/', views.user_login, name='login'),
     path('login/', auth_view.LoginView.as_view(), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(), name='logout'),
+    #path('logout/', auth_view.LogoutView.as_view(), name='logout'),
+    path('logout/', user_logout, name='logout'),
     path('', views.dashboard, name='dashboard' ),
 ]
