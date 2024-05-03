143193f7532ce789c19162f99f33493bb4d46a60
mwandi <frankmwandi93@gmail.com>
2024-05-03 14:59:50 +0300

    used inbuilt urls include

--- account/urls.py
+++ account/urls.py
@@ -1,14 +1,11 @@
-from django.urls import path
-from django.contrib.auth import views as auth_view
-from . import views 
-from .views import user_logout
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
 
 urlpatterns = [
-    # using custom viewpath('login/', views.user_login, name='login'),
-    path('login/', auth_view.LoginView.as_view(), name='login'),
-    #path('logout/', auth_view.LogoutView.as_view(), name='logout'),
-    path('logout/', user_logout, name='logout'),
-    path('password-change/', auth_view.PasswordChangeView.as_view(), name='password_change'),
-    path('password-change/done/', auth_view.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('', views.dashboard, name='dashboard' ),
+    path('', views.dashboard, name='dashboard'),  # separate URL pattern for the dashboard
+    path('logout/', views.user_logout, name='logout' ),
+    path('', include('django.contrib.auth.urls')),  # Include Django's authentication views
 ]
