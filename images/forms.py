f1bf0109c797b5b9fda864502f78dfa1ff1b53d4
mwandi <frankmwandi93@gmail.com>
2024-05-09 23:42:06 +0300

    image form for posting shared image

---
+++ images/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Image 
+
+class ImageCreateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Image 
+        fields = ['title', 'url', 'description']
+        widget = {
+            'url':forms.HiddenInput,
+        }
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg','jpeg','png']
+        extension = url.split('.',1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('Url provide does not match the required image extension')
+        return url
